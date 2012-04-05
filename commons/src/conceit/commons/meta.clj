8e9971cba4efe1f8f1e59a86ee99487f59ccb348
Yuu Shimizu <yuu@nyampass.com>
2012-04-05 12:43:03 +0900

    [commons] Remove `meta?`

--- commons/src/conceit/commons/meta.clj
+++ commons/src/conceit/commons/meta.clj
@@ -1,7 +1,4 @@
 (ns conceit.commons.meta)
 
-(defn meta? [obj]
-  (instance? clojure.lang.IMeta obj))
-
 (defn assoc-meta [obj & kvs]
   (apply vary-meta obj assoc kvs))
--- commons/test/conceit/commons/test/meta.clj
+++ commons/test/conceit/commons/test/meta.clj
@@ -3,15 +3,6 @@
        conceit.commons.test
        clojure.test))
 
-(deftest* meta?-test
-  (meta? {:a 10 :b 20})
-  (meta? [1 2 3])
-  (meta? (list :a :b :c))
-  (not (meta? 100))
-  (not (meta? :foo))
-  (not (meta? "aaa"))
-  (not (meta? (Object.))))
-
 (deftest* assoc-meta-test
   (let [v (assoc-meta (with-meta {:a 50} {:x 1}) :y 2)]
     (= {:a 50} v)
