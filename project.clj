3b3f6a8109e47a8f4766c5fea6bff9ef136027d6
Yuu Shimizu <yuu@nyampass.com>
2012-04-05 14:29:10 +0900

    Fix `README` and `project.cj`

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A web application framework in Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [conceit/commons "1.0.0"]]
-  :sub ["commons"])
+  :sub ["commons"]
+  :aot [conceit.core])
