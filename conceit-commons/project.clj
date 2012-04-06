a4c07e67005b6c30b627be9f9eaa142b0b7de4fd
Yuu Shimizu <yuu@nyampass.com>
2012-04-06 15:21:41 +0900

    [commons] Rename the sub project `commons` to `conceit-commons`

--- conceit-commons/project.clj
+++ conceit-commons/project.clj
@@ -1,4 +1,4 @@
-(defproject conceit/commons "1.0.0"
+(defproject conceit/conceit-commons "1.0.0"
   :description "Common libraries for Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [javax.mail/mail "1.4.3"]
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject conceit "1.0.0"
   :description "A web application framework in Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [conceit/commons "1.0.0"]]
-  :sub ["commons"]
+                 [conceit/conceit-commons "1.0.0"]]
+  :sub ["conceit-commons"]
   :aot [conceit.core])
