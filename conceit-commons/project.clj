b44f9dde2e1d4d64068b5e449a14d1f2ce023c7e
Yuu Shimizu <yuu@nyampass.com>
2012-07-11 11:55:37 +0900

    [commons] Release 1.0.15: Change the version of Mongoika used

--- conceit-commons/project.clj
+++ conceit-commons/project.clj
@@ -1,8 +1,8 @@
-(defproject conceit/conceit-commons "1.0.14"
+(defproject conceit/conceit-commons "1.0.15"
   :description "Common libraries for Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [javax.mail/mail "1.4.3"]
-                 [mongoika "0.6.8"]]
+                 [mongoika "0.6.12"]]
   :dev-dependencies [[lein-ring "0.4.6"]
                      [swank-clojure "1.4.0"]]
   :aot [conceit.commons])
