7af93f8bc9406cc6053554050db36223527ef8ff
Yuu Shimizu <yuu@nyampass.com>
2012-12-27 17:34:22 +0900

    [commons] Remove libraries about mail and MongoDB

--- conceit-commons/project.clj
+++ conceit-commons/project.clj
@@ -1,8 +1,6 @@
-(defproject conceit/conceit-commons "1.0.21"
+(defproject conceit/conceit-commons "1.0.22"
   :description "Common libraries for Clojure."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [javax.mail/mail "1.4.3"]
-                 [mongoika "0.6.12"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[lein-ring "0.4.6"]
                      [swank-clojure "1.4.0"]]
   :aot [conceit.commons
@@ -11,6 +9,4 @@
         conceit.commons.digest
         conceit.commons.file
         conceit.commons.http
-        conceit.commons.image
-        conceit.commons.mail
-        conceit.commons.mongodb])
+        conceit.commons.image])
--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "A web application framework in Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [conceit/conceit-commons "1.0.0"]]
-  :sub ["conceit-commons"]
+  :sub ["conceit-commons" "conceit-validation"]
   :aot [conceit.core])
